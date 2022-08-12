993e6db64cdf640b7e3ee8b15a1ee1d88a43ca15
victord03 <vic.kaklamanis@gmail.com>
2022-08-12 12:16:17 +0300

    Project restructuring successful. Created project structure data, src, classes. Created data file for armor sets.

--- classes/project_classes.py
+++ classes/project_classes.py
@@ -1,27 +1,23 @@
 
 class ArmorPiece:
-    # name: str
-    # armor_piece: str
+    armor_piece: int
     physical: int
     magical: int
     fire: int
     lighting: int
     price: int
 
-    def __init__(
-            self, name: str, armor_piece: int, physical: int, magical: int, fire: int, lighting: int, price: int
-    ) -> None:
-        self.name = name
-        self.armor_piece = armor_piece
-        self.physical = physical
-        self.magical = magical
-        self.fire = fire
-        self.lighting = lighting
-        self.price = price
+    def __init__(self, data_dict: dict) -> None:
+        self.armor_piece = data_dict["armor_piece"]
+        self.physical = data_dict["physical"]
+        self.magical = data_dict["magical"]
+        self.fire = data_dict["fire"]
+        self.lighting = data_dict["lightning"]
+        self.price = data_dict["price"]
 
     def __repr__(self) -> str:
         return f"\n\t\tPHYS {self.physical}\n\t\tMAG {self.magical}\n\t\tFIRE {self.fire}\n\t\tLIGHT" \
-               + f"{self.lighting}\n\t\tPRICE {self.price:,}"
+               + f" {self.lighting}\n\t\tPRICE {self.price:,}"
 
 
 class ArmorSet:
@@ -31,26 +27,22 @@ class ArmorSet:
     gauntlet: ArmorPiece
     leggings: ArmorPiece
 
-    def __init__(self, name: str):
-        self.name = name
+    def __init__(self, data_dict: dict) -> None:
+        self.name = data_dict["name"]
+        self.helmet = ArmorPiece(data_dict["helmet"])
+        self.body = ArmorPiece(data_dict["body"])
+        self.gauntlet = ArmorPiece(data_dict["gauntlet"])
+        self.leggings = ArmorPiece(data_dict["leggings"])
 
     def __repr__(self) -> str:
         return f"\n{self.name} Armor Set\n\n\tHELMET{self.helmet.__repr__()}" \
                + f"\n\n\tBODY{self.body.__repr__()}\n\n\tGAUNTLET{self.gauntlet.__repr__()}" \
                + f"\n\n\tLEGGINGS{self.leggings.__repr__()}"
 
-    def complete_set(self, helmet: ArmorPiece, body: ArmorPiece, gauntlet: ArmorPiece, leggings: ArmorPiece) -> None:
-        self.helmet = helmet
-        self.body = body
-        self.gauntlet = gauntlet
-        self.leggings = leggings
-
-    def compare_armor_sets(self, other: any):
+    def compare_armor_sets(self, armor_set) -> None:
 
         attributes = list(self.helmet.__dict__.keys())
         attributes_2 = [attribute for attribute in self.helmet.__dict__]
         print(attributes)
         print(attributes_2)
 
-        # for each in attributes:
-            # print(self.helmet.each)
--- data/armor_sets.py
+++ data/armor_sets.py
@@ -1,16 +1,90 @@
 
-steel_set_stats = {
-    "Name": "Steel",
-    "Helmet": (16, 8, 8, 5, 5_000),
-    "Body": (42, 22, 21, 13, 8_000),
-    "Gauntlet": (25, 13, 12, 8, 5_000),
-    "Leggings": (25, 13, 12, 8, 5_000)
+steel_helmet = {
+    "armor_piece": "Helmet",
+    "physical": 16,
+    "magical": 8,
+    "fire": 8,
+    "lightning": 5,
+    "price": 5_000
 }
 
-balder_set_stats = {
-    "Name": "Balder",
-    "Helmet": (14, 6, 8, 6, 5_000),
-    "Body": (37, 15, 18, 15, 8_000),
-    "Gauntlet": (17, 7, 8, 5, 5_000),
-    "Leggings": (22, 10, 12, 9, 5_000),
+steel_body = {
+    "armor_piece": "Body",
+    "physical": 42,
+    "magical": 22,
+    "fire": 21,
+    "lightning": 13,
+    "price": 8_000,
+}
+
+steel_gauntlet = {
+    "armor_piece": "Gauntlet",
+    "physical": 25,
+    "magical": 13,
+    "fire": 12,
+    "lightning": 8,
+    "price": 5_000,
+}
+
+steel_leggings = {
+    "armor_piece": "Leggings",
+    "physical": 25,
+    "magical": 13,
+    "fire": 12,
+    "lightning": 8,
+    "price": 5_000,
+}
+
+
+steel_set = {
+    "name": "Steel",
+    "helmet": steel_helmet,
+    "body": steel_body,
+    "gauntlet": steel_gauntlet,
+    "leggings": steel_leggings
+}
+
+
+balder_helmet = {
+    "armor_piece": "Helmet",
+    "physical": 14,
+    "magical": 6,
+    "fire": 8,
+    "lightning": 6,
+    "price": 5_000
+}
+
+balder_body = {
+    "armor_piece": "Body",
+    "physical": 37,
+    "magical": 15,
+    "fire": 18,
+    "lightning": 15,
+    "price": 8_000
+}
+
+balder_gauntlet = {
+    "armor_piece": "Gauntlet",
+    "physical": 17,
+    "magical": 7,
+    "fire": 8,
+    "lightning": 5,
+    "price": 5_000
+}
+
+balder_leggings = {
+    "armor_piece": "Leggings",
+    "physical": 22,
+    "magical": 10,
+    "fire": 12,
+    "lightning": 9,
+    "price": 5_000
+}
+
+balder_set = {
+    "name": "Balder",
+    "helmet": balder_helmet,
+    "body": balder_body,
+    "gauntlet": balder_gauntlet,
+    "leggings": balder_leggings,
 }
